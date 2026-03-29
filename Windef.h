9141bee182ac5ff365451608636b396eb6419d27
Money <underpopo77@gmail.com>
2026-03-29 02:04:38 +0100

    Improved some stuff and added a simple function that finds the section for the import Directory

---
+++ Windef.h
@@ -0,0 +1,121 @@
+#pragma once
+#include <windows.h>
+    typedef struct DOS_HEADER {
+      WORD e_magic;
+      WORD e_cblp;
+      WORD e_cp;
+      WORD e_crlc;
+      WORD e_cparhdr;
+      WORD e_minalloc;
+      WORD e_maxalloc;
+      WORD e_ss;
+      WORD e_sp;
+      WORD e_csum;
+      WORD e_ip;
+      WORD e_cs;
+      WORD e_lfarlc;
+      WORD e_ovno;
+      WORD e_res[4];
+      WORD e_oemid;
+      WORD e_oeminfo;
+      WORD e_res2[10];
+      LONG e_lfanew;
+    } DOS_HEADER,*PDOS_HEADER;
+
+
+
+
+typedef struct FILE_HEADER {
+  DWORD Signature;
+  WORD Machine;
+  WORD  NumberOfSections;
+  DWORD TimeDateStamp;
+  DWORD PointerToSymbolTable;
+  DWORD NumberOfSymbols;
+  WORD  SizeOfOptionalHeader;
+  WORD  Characteristics;
+} FILE_HEADER, *PFILE_HEADER;   
+
+
+typedef struct OPTIONAL_HEADER {
+  WORD                 Magic;
+  BYTE                 MajorLinkerVersion;
+  BYTE                 MinorLinkerVersion;
+  DWORD                SizeOfCode;
+  DWORD                SizeOfInitializedData;
+  DWORD                SizeOfUninitializedData;
+  DWORD                AddressOfEntryPoint;
+  DWORD                BaseOfCode;
+  DWORD                BaseOfData;
+  DWORD                ImageBase;
+  DWORD                SectionAlignment;
+  DWORD                FileAlignment;
+  WORD                 MajorOperatingSystemVersion;
+  WORD                 MinorOperatingSystemVersion;
+  WORD                 MajorImageVersion;
+  WORD                 MinorImageVersion;
+  WORD                 MajorSubsystemVersion;
+  WORD                 MinorSubsystemVersion;
+  DWORD                Win32VersionValue;
+  DWORD                SizeOfImage;
+  DWORD                SizeOfHeaders;
+  DWORD                CheckSum;
+  WORD                 Subsystem;
+  WORD                 DllCharacteristics;
+  DWORD                SizeOfStackReserve;
+  DWORD                SizeOfStackCommit;
+  DWORD                SizeOfHeapReserve;
+  DWORD                SizeOfHeapCommit;
+  DWORD                LoaderFlags;
+  DWORD                NumberOfRvaAndSizes;
+  IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} OPTIONAL_HEADER32, *POPTIONAL_HEADER32;
+
+typedef struct OPTIONAL_HEADER64 {
+  WORD                 Magic;
+  BYTE                 MajorLinkerVersion;
+  BYTE                 MinorLinkerVersion;
+  DWORD                SizeOfCode;
+  DWORD                SizeOfInitializedData;
+  DWORD                SizeOfUninitializedData;
+  DWORD                AddressOfEntryPoint;
+  DWORD                BaseOfCode;
+  ULONGLONG            ImageBase;
+  DWORD                SectionAlignment;
+  DWORD                FileAlignment;
+  WORD                 MajorOperatingSystemVersion;
+  WORD                 MinorOperatingSystemVersion;
+  WORD                 MajorImageVersion;
+  WORD                 MinorImageVersion;
+  WORD                 MajorSubsystemVersion;
+  WORD                 MinorSubsystemVersion;
+  DWORD                Win32VersionValue;
+  DWORD                SizeOfImage;
+  DWORD                SizeOfHeaders;
+  DWORD                CheckSum;
+  WORD                 Subsystem;
+  WORD                 DllCharacteristics;
+  ULONGLONG            SizeOfStackReserve;
+  ULONGLONG            SizeOfStackCommit;
+  ULONGLONG            SizeOfHeapReserve;
+  ULONGLONG            SizeOfHeapCommit;
+  DWORD                LoaderFlags;
+  DWORD                NumberOfRvaAndSizes;
+  IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} OPTIONAL_HEADER64, *POPTIONAL_HEADER64;
+ 
+typedef struct SECTION_HEADER {
+  BYTE  Name[IMAGE_SIZEOF_SHORT_NAME];
+  union {
+    DWORD PhysicalAddress;
+    DWORD VirtualSize;
+  } Misc;
+  DWORD VirtualAddress;
+  DWORD SizeOfRawData;
+  DWORD PointerToRawData;
+  DWORD PointerToRelocations;
+  DWORD PointerToLinenumbers;
+  WORD  NumberOfRelocations;
+  WORD  NumberOfLinenumbers;
+  DWORD Characteristics;
+} SECTION_HEADER, *PSECTION_HEADER;
---
+++ main.cpp
@@ -0,0 +1,103 @@
+#include <print>
+#include <vector>
+#include "Windef.h"
+
+using std::print, std::println;
+
+_IMAGE_DOS_HEADER DosHeader;
+FILE_HEADER FileHeader;
+OPTIONAL_HEADER32 Optional_Header;
+std::vector<SECTION_HEADER> SECTION_TABLE;
+
+enum DATA_DIR
+{
+    EXPORT,
+    IMPORT,
+    RESOURCE,
+    EXCEPTION,
+    SECURITY,
+    BASE_RELOCATION_TABLE,
+    DEBUG_DIRECTORY,
+    COPYRIGHT_SPECIFIC_DATA,
+    GLOBAL_PTR,
+    TLS_DIRECTORY,
+    LOAD_CONFIG_DIRECTORY,
+    BOUND_IMPORT_DIRECTORY,
+    IMPORT_ADDRESS_TABLE,
+    DLLD,
+    CLR_DIRECTORY,
+    RESERVED
+};
+
+auto
+Read_Dos_Header(HANDLE &File) -> void
+{
+    SetFilePointer(File, 0, 0, FILE_BEGIN);
+    if (!ReadFile(File, &DosHeader, sizeof(_IMAGE_DOS_HEADER), 0, 0))
+    {
+        println("Failed to read file 0x{:0X}", GetLastError());
+        exit(1);
+    }
+}
+
+auto Read_File_Header(HANDLE &File) -> void
+{
+    SetFilePointer(File, DosHeader.e_lfanew, 0, FILE_BEGIN);
+    if (!ReadFile(File, &FileHeader, sizeof(FILE_HEADER), 0, 0))
+    {
+        println("Failed to read file 0x{:0X}", GetLastError());
+        exit(1);
+    }
+}
+
+auto Read_Optional_Header(HANDLE &File) -> void
+{
+    SetFilePointer(File, DosHeader.e_lfanew + sizeof(FILE_HEADER), 0, FILE_BEGIN);
+    if (!ReadFile(File, &Optional_Header, sizeof(Optional_Header), 0, 0))
+    {
+        println("Failed to read file 0x{:0X}", GetLastError());
+        exit(1);
+    }
+    SetFilePointer(File, DosHeader.e_lfanew + sizeof(FILE_HEADER) + FileHeader.SizeOfOptionalHeader, 0, FILE_BEGIN);
+}
+auto Read_Section_Table(HANDLE &File) -> void
+{
+    SECTION_TABLE.resize(FileHeader.NumberOfSections);
+    if (!ReadFile(File, SECTION_TABLE.data(), (sizeof(SECTION_HEADER) * FileHeader.NumberOfSections), 0, 0))
+    {
+        println("Failed to read section header 0x{:0X}", GetLastError());
+        exit(1);
+    }
+}
+
+auto Import_Table_Section() -> int
+{
+    for (int i = 0; i < FileHeader.NumberOfSections; i++)
+    {
+        DWORD Section_Rva = SECTION_TABLE[i].VirtualAddress;
+        DWORD IMPORT_RVA = Optional_Header.DataDirectory[1].VirtualAddress;
+        if (Section_Rva <=  && IMPORT_RVA <= (Section_Rva + SECTION_TABLE[i].Misc.VirtualSize))
+        {
+            println("Found at Section {}", (char *)&SECTION_TABLE[i].Name);
+            return i;
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    HANDLE File = CreateFileA("Read.exe", GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+    if (File == INVALID_HANDLE_VALUE)
+    {
+        println("Failed to open file 0x{:0X}", GetLastError());
+        return GetLastError();
+    }
+    Read_Dos_Header(File);
+    Read_File_Header(File);
+    Read_Optional_Header(File);
+    Read_Section_Table(File);
+
+
+    return 0;
+}
