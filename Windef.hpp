0aca7f9a17bf1d11ceb56fffea288af67d3188dd
hazouka <hazoukabe@gmail.com>
2026-04-25 22:23:53 +0100

    modified:   Windef.hpp 	modified:   main.cpp

--- Windef.hpp
+++ Windef.hpp
@@ -1,112 +1,131 @@
 #pragma once
-#include <windows.h>
-    typedef struct DOS_HEADER {
-      WORD e_magic;
-      WORD e_cblp;
-      WORD e_cp;
-      WORD e_crlc;
-      WORD e_cparhdr;
-      WORD e_minalloc;
-      WORD e_maxalloc;
-      WORD e_ss;
-      WORD e_sp;
-      WORD e_csum;
-      WORD e_ip;
-      WORD e_cs;
-      WORD e_lfarlc;
-      WORD e_ovno;
-      WORD e_res[4];
-      WORD e_oemid;
-      WORD e_oeminfo;
-      WORD e_res2[10];
-      LONG e_lfanew;
-    } DOS_HEADER,*PDOS_HEADER;
+#include <cstdint>
 
+using WORD = uint16_t;
+using DWORD = uint32_t;
+using BYTE = uint8_t;
+using LONG = int32_t;
+using ULONGLONG = uint64_t;
+#define PE32 0x10B
+#define PE64 0x20B
+#define IMAGE_NUMBEROF_DIRECTORY_ENTRIES 15
+#define IMAGE_SIZEOF_SHORT_NAME 7
 
+typedef struct _IMAGE_DOS_HEADER
+{
+  WORD e_magic;
+  WORD e_cblp;
+  WORD e_cp;
+  WORD e_crlc;
+  WORD e_cparhdr;
+  WORD e_minalloc;
+  WORD e_maxalloc;
+  WORD e_ss;
+  WORD e_sp;
+  WORD e_csum;
+  WORD e_ip;
+  WORD e_cs;
+  WORD e_lfarlc;
+  WORD e_ovno;
+  WORD e_res[4];
+  WORD e_oemid;
+  WORD e_oeminfo;
+  WORD e_res2[10];
+  DWORD e_lfanew;
+} IMAGE_DOS_HEADER, *PIMAGE_DOS_HEADER;
 
-
-typedef struct FILE_HEADER {
+typedef struct FILE_HEADER
+{
   DWORD Signature;
   WORD Machine;
-  WORD  NumberOfSections;
+  WORD NumberOfSections;
   DWORD TimeDateStamp;
   DWORD PointerToSymbolTable;
   DWORD NumberOfSymbols;
-  WORD  SizeOfOptionalHeader;
-  WORD  Characteristics;
-} FILE_HEADER, *PFILE_HEADER;   
+  WORD SizeOfOptionalHeader;
+  WORD Characteristics;
+} FILE_HEADER, *PFILE_HEADER;
 
+typedef struct _IMAGE_DATA_DIRECTORY
+{
+  DWORD VirtualAddress;
+  DWORD Size;
+} IMAGE_DATA_DIRECTORY, *PIMAGE_DATA_DIRECTORY;
 
-typedef struct OPTIONAL_HEADER {
-  WORD                 Magic;
-  BYTE                 MajorLinkerVersion;
-  BYTE                 MinorLinkerVersion;
-  DWORD                SizeOfCode;
-  DWORD                SizeOfInitializedData;
-  DWORD                SizeOfUninitializedData;
-  DWORD                AddressOfEntryPoint;
-  DWORD                BaseOfCode;
-  DWORD                BaseOfData;
-  DWORD                ImageBase;
-  DWORD                SectionAlignment;
-  DWORD                FileAlignment;
-  WORD                 MajorOperatingSystemVersion;
-  WORD                 MinorOperatingSystemVersion;
-  WORD                 MajorImageVersion;
-  WORD                 MinorImageVersion;
-  WORD                 MajorSubsystemVersion;
-  WORD                 MinorSubsystemVersion;
-  DWORD                Win32VersionValue;
-  DWORD                SizeOfImage;
-  DWORD                SizeOfHeaders;
-  DWORD                CheckSum;
-  WORD                 Subsystem;
-  WORD                 DllCharacteristics;
-  DWORD                SizeOfStackReserve;
-  DWORD                SizeOfStackCommit;
-  DWORD                SizeOfHeapReserve;
-  DWORD                SizeOfHeapCommit;
-  DWORD                LoaderFlags;
-  DWORD                NumberOfRvaAndSizes;
+typedef struct OPTIONAL_HEADER
+{
+  WORD Magic;
+  BYTE MajorLinkerVersion;
+  BYTE MinorLinkerVersion;
+  DWORD SizeOfCode;
+  DWORD SizeOfInitializedData;
+  DWORD SizeOfUninitializedData;
+  DWORD AddressOfEntryPoint;
+  DWORD BaseOfCode;
+  DWORD BaseOfData;
+  DWORD ImageBase;
+  DWORD SectionAlignment;
+  DWORD FileAlignment;
+  WORD MajorOperatingSystemVersion;
+  WORD MinorOperatingSystemVersion;
+  WORD MajorImageVersion;
+  WORD MinorImageVersion;
+  WORD MajorSubsystemVersion;
+  WORD MinorSubsystemVersion;
+  DWORD Win32VersionValue;
+  DWORD SizeOfImage;
+  DWORD SizeOfHeaders;
+  DWORD CheckSum;
+  WORD Subsystem;
+  WORD DllCharacteristics;
+  DWORD SizeOfStackReserve;
+  DWORD SizeOfStackCommit;
+  DWORD SizeOfHeapReserve;
+  DWORD SizeOfHeapCommit;
+  DWORD LoaderFlags;
+  DWORD NumberOfRvaAndSizes;
   IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
 } OPTIONAL_HEADER, *POPTIONAL_HEADER;
 
-typedef struct OPTIONAL_HEADER64 {
-  WORD                 Magic;
-  BYTE                 MajorLinkerVersion;
-  BYTE                 MinorLinkerVersion;
-  DWORD                SizeOfCode;
-  DWORD                SizeOfInitializedData;
-  DWORD                SizeOfUninitializedData;
-  DWORD                AddressOfEntryPoint;
-  DWORD                BaseOfCode;
-  ULONGLONG            ImageBase;
-  DWORD                SectionAlignment;
-  DWORD                FileAlignment;
-  WORD                 MajorOperatingSystemVersion;
-  WORD                 MinorOperatingSystemVersion;
-  WORD                 MajorImageVersion;
-  WORD                 MinorImageVersion;
-  WORD                 MajorSubsystemVersion;
-  WORD                 MinorSubsystemVersion;
-  DWORD                Win32VersionValue;
-  DWORD                SizeOfImage;
-  DWORD                SizeOfHeaders;
-  DWORD                CheckSum;
-  WORD                 Subsystem;
-  WORD                 DllCharacteristics;
-  ULONGLONG            SizeOfStackReserve;
-  ULONGLONG            SizeOfStackCommit;
-  ULONGLONG            SizeOfHeapReserve;
-  ULONGLONG            SizeOfHeapCommit;
-  DWORD                LoaderFlags;
-  DWORD                NumberOfRvaAndSizes;
+typedef struct OPTIONAL_HEADER64
+{
+  WORD Magic;
+  BYTE MajorLinkerVersion;
+  BYTE MinorLinkerVersion;
+  DWORD SizeOfCode;
+  DWORD SizeOfInitializedData;
+  DWORD SizeOfUninitializedData;
+  DWORD AddressOfEntryPoint;
+  DWORD BaseOfCode;
+  ULONGLONG ImageBase;
+  DWORD SectionAlignment;
+  DWORD FileAlignment;
+  WORD MajorOperatingSystemVersion;
+  WORD MinorOperatingSystemVersion;
+  WORD MajorImageVersion;
+  WORD MinorImageVersion;
+  WORD MajorSubsystemVersion;
+  WORD MinorSubsystemVersion;
+  DWORD Win32VersionValue;
+  DWORD SizeOfImage;
+  DWORD SizeOfHeaders;
+  DWORD CheckSum;
+  WORD Subsystem;
+  WORD DllCharacteristics;
+  ULONGLONG SizeOfStackReserve;
+  ULONGLONG SizeOfStackCommit;
+  ULONGLONG SizeOfHeapReserve;
+  ULONGLONG SizeOfHeapCommit;
+  DWORD LoaderFlags;
+  DWORD NumberOfRvaAndSizes;
   IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
 } OPTIONAL_HEADER64, *POPTIONAL_HEADER64;
- 
-typedef struct SECTION_HEADER {
-  BYTE  Name[IMAGE_SIZEOF_SHORT_NAME];
-  union {
+
+typedef struct SECTION_HEADER
+{
+  BYTE Name[IMAGE_SIZEOF_SHORT_NAME];
+  union
+  {
     DWORD PhysicalAddress;
     DWORD VirtualSize;
   } Misc;
@@ -115,36 +134,37 @@ typedef struct SECTION_HEADER {
   DWORD PointerToRawData;
   DWORD PointerToRelocations;
   DWORD PointerToLinenumbers;
-  WORD  NumberOfRelocations;
-  WORD  NumberOfLinenumbers;
+  WORD NumberOfRelocations;
+  WORD NumberOfLinenumbers;
   DWORD Characteristics;
 } SECTION_HEADER, *PSECTION_HEADER;
 
+typedef struct IMG_IMPORT_DESCRIPTOR
+{
+  union
+  {
+    DWORD Characteristics;
+    DWORD OriginalFirstThunk;
+  } DUMMYUNIONNAME;
+  DWORD TimeDateStamp;
 
+  DWORD ForwarderChain;
+  DWORD Name;
+  DWORD FirstThunk;
+} IMG_IMPORT_DESCRIPTOR;
+typedef IMG_IMPORT_DESCRIPTOR *PIMG_IMPORT_DESCRIPTOR;
 
-typedef struct IMG_IMPORT_DESCRIPTOR {
-      __C89_NAMELESS union {
-	DWORD Characteristics;
-	DWORD OriginalFirstThunk;
-      } DUMMYUNIONNAME;
-      DWORD TimeDateStamp;
-
-      DWORD ForwarderChain;
-      DWORD Name;
-      DWORD FirstThunk;
-    }   IMG_IMPORT_DESCRIPTOR;
-    typedef IMG_IMPORT_DESCRIPTOR UNALIGNED *PIMG_IMPORT_DESCRIPTOR;
-
-typedef struct IMG_EXPORT_DIRECTORY {
-      DWORD Characteristics;
-      DWORD TimeDateStamp;
-      WORD MajorVersion;
-      WORD MinorVersion;
-      DWORD Name;
-      DWORD Base;
-      DWORD NumberOfFunctions;
-      DWORD NumberOfNames;
-      DWORD AddressOfFunctions;
-      DWORD AddressOfNames;
-      DWORD AddressOfNameOrdinals;
-    } IMG_EXPORT_DIRECTORY,*PIMG_EXPORT_DIRECTORY;
+typedef struct IMG_EXPORT_DIRECTORY
+{
+  DWORD Characteristics;
+  DWORD TimeDateStamp;
+  WORD MajorVersion;
+  WORD MinorVersion;
+  DWORD Name;
+  DWORD Base;
+  DWORD NumberOfFunctions;
+  DWORD NumberOfNames;
+  DWORD AddressOfFunctions;
+  DWORD AddressOfNames;
+  DWORD AddressOfNameOrdinals;
+} IMG_EXPORT_DIRECTORY, *PIMG_EXPORT_DIRECTORY;
--- main.cpp
+++ main.cpp
@@ -1,227 +1,308 @@
-#include <print>
-#include <vector>
-#include <ranges>
 #include "Windef.hpp"
+#include <fstream>
+#include <print>
+#include <ranges>
+#include <variant>
+#include <vector>
 using std::print, std::println;
+
 enum DATA_DIR
 {
-    EXPORT,
-    IMPORT,
-    RESOURCE,
-    EXCEPTION,
-    SECURITY,
-    BASE_RELOCATION_TABLE,
-    DEBUG_DIRECTORY,
-    COPYRIGHT_SPECIFIC_DATA,
-    GLOBAL_PTR,
-    TLS_DIRECTORY,
-    LOAD_CONFIG_DIRECTORY,
-    BOUND_IMPORT_DIRECTORY,
-    IMPORT_ADDRESS_TABLE,
-    DLLD,
-    CLR_DIRECTORY,
-    RESERVED
+  EXPORT,
+  IMPORT,
+  RESOURCE,
+  EXCEPTION,
+  SECURITY,
+  BASE_RELOCATION_TABLE,
+  DEBUG_DIRECTORY,
+  COPYRIGHT_SPECIFIC_DATA,
+  GLOBAL_PTR,
+  TLS_DIRECTORY,
+  LOAD_CONFIG_DIRECTORY,
+  BOUND_IMPORT_DIRECTORY,
+  IMPORT_ADDRESS_TABLE,
+  DLLD,
+  CLR_DIRECTORY,
+  RESERVED
 };
 
 _IMAGE_DOS_HEADER DosHeader;
 FILE_HEADER FileHeader;
+std::variant<OPTIONAL_HEADER, OPTIONAL_HEADER64> OptHeader;
 std::vector<SECTION_HEADER> SECTION_TABLE;
-OPTIONAL_HEADER64 OptionalHeader; // OPTIONAL_HEADER64 for 64bit
 std::vector<IMG_IMPORT_DESCRIPTOR> IMG_DESCRIPTOR;
 IMG_EXPORT_DIRECTORY EXPORT_DIRECTORY;
-HANDLE File;
+
+std::ifstream File;
+
 DWORD Offset_Functions = 0;
-DWORD Offset_Ordinals = 0;
-DWORD Offset_Names = 0;
+DWORD Offset_Ordinals  = 0;
+DWORD Offset_Names     = 0;
+
 std::vector<std::string> names;
-std::vector<DWORD> EXPORT_TABLE;
-std::vector<DWORD> EXPORT_NAME_TABLE;
-std::vector<WORD> EXPORT_ORDINAL_TABLE;
+std::vector<DWORD>       EXPORT_TABLE;
+std::vector<DWORD>       EXPORT_NAME_TABLE;
+std::vector<WORD>        EXPORT_ORDINAL_TABLE;
+
 
 auto ReadDosHeader() -> void
 {
-    SetFilePointer(File, 0, 0, FILE_BEGIN);
-    if (!ReadFile(File, &DosHeader, sizeof(_IMAGE_DOS_HEADER), 0, 0))
-    {
-        println("failed to read DOS header 0x{:0X}", GetLastError());
-        exit(1);
-    }
+  File.seekg(std::ios_base::beg);
+  if (!File.read(reinterpret_cast<char*>(&DosHeader), sizeof(_IMAGE_DOS_HEADER)))
+  {
+    println("Failed to read the Dos Header");
+    exit(1);
+  }
 }
 
 auto ReadFileHeader() -> void
 {
-    SetFilePointer(File, DosHeader.e_lfanew, 0, FILE_BEGIN);
-    if (!ReadFile(File, &FileHeader, sizeof(FILE_HEADER), 0, 0))
-    {
-        println("failed to read file header 0x{:0X}", GetLastError());
-        exit(1);
-    }
+  File.seekg(DosHeader.e_lfanew, std::ios::beg);
+  if (!File.read(reinterpret_cast<char*>(&FileHeader), sizeof(FILE_HEADER)))
+  {
+    println("Failed to read the File Header");
+    exit(1);
+  }
 }
 
-auto ReadOptionalHeader() -> void
+
+auto ReadNtHeaders() -> void
 {
-    SetFilePointer(File, DosHeader.e_lfanew + sizeof(FILE_HEADER), 0, FILE_BEGIN);
-    if (!ReadFile(File, &OptionalHeader, sizeof(OptionalHeader), 0, 0))
+  ReadDosHeader();
+  ReadFileHeader();
+
+  File.seekg(DosHeader.e_lfanew + sizeof(FILE_HEADER), std::ios::beg);
+  WORD Magic;
+  if (!File.read(reinterpret_cast<char*>(&Magic), sizeof(WORD)))
+  {
+    println("Failed to read PE magic");
+    exit(1);
+  }
+
+
+  File.seekg(DosHeader.e_lfanew + sizeof(FILE_HEADER), std::ios::beg);
+
+  if (Magic == PE32)
+  {
+    OPTIONAL_HEADER header{};
+    if (!File.read(reinterpret_cast<char*>(&header), sizeof(OPTIONAL_HEADER)))
     {
-        println("failed to read optional header 0x{:0X}", GetLastError());
-        exit(1);
+      println("Failed to read optional header (PE32)");
+      exit(1);
     }
+    OptHeader = header;
+  }
+  else if (Magic == PE64)
+  {
+    OPTIONAL_HEADER64 header{};
+    if (!File.read(reinterpret_cast<char*>(&header), sizeof(OPTIONAL_HEADER64)))
+    {
+      println("Failed to read optional header (PE64)");
+      exit(1);
+    }
+    OptHeader = header;
+  }
+  else
+  {
+    println("Unknown PE magic: {:X}", Magic);
+    exit(1);
+  }
 }
+
+
 auto ReadSectionTable() -> void
 {
-    SECTION_TABLE.resize(FileHeader.NumberOfSections);
-    if (!ReadFile(File, SECTION_TABLE.data(), (sizeof(SECTION_HEADER) * FileHeader.NumberOfSections), 0, 0))
-    {
-        println("failed to read section table 0x{:0X}", GetLastError());
-        exit(1);
-    }
+  File.seekg(DosHeader.e_lfanew + sizeof(FILE_HEADER) + FileHeader.SizeOfOptionalHeader, std::ios::beg);
+  SECTION_TABLE.resize(FileHeader.NumberOfSections);
+  if (!File.read(reinterpret_cast<char*>(SECTION_TABLE.data()),
+                 sizeof(SECTION_HEADER) * FileHeader.NumberOfSections))
+  {
+    println("Failed to read section table");
+    exit(1);
+  }
 }
 
-auto CalculateSectionLocation(const DWORD &RVA) -> int
+auto CalculateSectionLocation(const DWORD& RVA) -> int
 {
-    for (int i = 0; i < FileHeader.NumberOfSections; i++)
-    {
-        DWORD Section_Rva = SECTION_TABLE[i].VirtualAddress;
-        if (Section_Rva <= RVA && RVA <= (Section_Rva + SECTION_TABLE[i].Misc.VirtualSize))
-        {
-            return i;
-        }
-    }
-    return -1;
+  for (int i = 0; i < FileHeader.NumberOfSections; i++)
+  {
+    DWORD Section_Rva = SECTION_TABLE[i].VirtualAddress;
+    if (Section_Rva <= RVA && RVA <= (Section_Rva + SECTION_TABLE[i].Misc.VirtualSize))
+      return i;
+  }
+  return -1;
 }
+
 
 auto ReadImportDescriptor() -> void
 {
-    int IT_SECTION_INDEX = CalculateSectionLocation(OptionalHeader.DataDirectory[IMPORT].VirtualAddress);
-    DWORD Offset = OptionalHeader.DataDirectory[IMPORT].VirtualAddress - SECTION_TABLE[IT_SECTION_INDEX].VirtualAddress + SECTION_TABLE[IT_SECTION_INDEX].PointerToRawData;
-    SetFilePointer(File, Offset, 0, FILE_BEGIN);
+  std::visit([](auto& header) {
+    int IT_SECTION_INDEX = CalculateSectionLocation(
+        header.DataDirectory[IMPORT].VirtualAddress);
+
+    DWORD Offset = header.DataDirectory[IMPORT].VirtualAddress -
+                   SECTION_TABLE[IT_SECTION_INDEX].VirtualAddress +
+                   SECTION_TABLE[IT_SECTION_INDEX].PointerToRawData;
+
+    File.seekg(Offset, std::ios_base::beg);
     while (true)
     {
-        IMG_IMPORT_DESCRIPTOR desc{};
-        if (!ReadFile(File, &desc, sizeof(IMG_IMPORT_DESCRIPTOR), 0, 0))
-        {
-            println("failed to read import descriptor 0x{:0X}", GetLastError());
-            exit(1);
-        }
-
-        if (desc.Name == 0)
-            break;
-
-        IMG_DESCRIPTOR.push_back(desc);
+      IMG_IMPORT_DESCRIPTOR desc{};
+      if (!File.read(reinterpret_cast<char*>(&desc), sizeof(IMG_IMPORT_DESCRIPTOR)))
+      {
+        println("Failed to read import descriptor");
+        exit(1);
+      }
+      if (desc.Name == 0)
+        break;
+      IMG_DESCRIPTOR.push_back(desc);
     }
+  }, OptHeader);
 }
+
 
 auto ReadExportDirectory() -> void
 {
-    int SECTION_INDEX = CalculateSectionLocation(OptionalHeader.DataDirectory[EXPORT].VirtualAddress);
+  std::visit([](auto& header) {
+    int SECTION_INDEX = CalculateSectionLocation(
+        header.DataDirectory[EXPORT].VirtualAddress);
+
     if (SECTION_INDEX == -1)
     {
-        println("failed to locate export directory section");
-        exit(1);
+      println("Failed to locate export directory section");
+      exit(1);
     }
-    DWORD Offset = OptionalHeader.DataDirectory[EXPORT].VirtualAddress - SECTION_TABLE[SECTION_INDEX].VirtualAddress + SECTION_TABLE[SECTION_INDEX].PointerToRawData;
-    SetFilePointer(File, Offset, 0, FILE_BEGIN);
-    if (!ReadFile(File, &EXPORT_DIRECTORY, sizeof(IMG_EXPORT_DIRECTORY), 0, 0))
-    {
-        println("failed to read export directory 0x{:0X}", GetLastError());
-        exit(1);
-    }
-}
-auto ReadExportTables() -> void
-{
-    EXPORT_TABLE.resize(EXPORT_DIRECTORY.NumberOfFunctions);
-    EXPORT_NAME_TABLE.resize(EXPORT_DIRECTORY.NumberOfNames);
-    EXPORT_ORDINAL_TABLE.resize(EXPORT_DIRECTORY.NumberOfNames);
 
-    SetFilePointer(File, Offset_Functions, 0, FILE_BEGIN);
-    if (!ReadFile(File, EXPORT_TABLE.data(), (sizeof(DWORD) * EXPORT_DIRECTORY.NumberOfFunctions), 0, 0))
+    DWORD Offset = header.DataDirectory[EXPORT].VirtualAddress -
+                   SECTION_TABLE[SECTION_INDEX].VirtualAddress +
+                   SECTION_TABLE[SECTION_INDEX].PointerToRawData;
+
+    File.seekg(Offset, std::ios_base::beg);
+    if (!File.read(reinterpret_cast<char*>(&EXPORT_DIRECTORY), sizeof(IMG_EXPORT_DIRECTORY)))
     {
-        println("failed to read function address table 0x{:0X}", GetLastError());
-        exit(1);
+      println("Failed to read export directory");
+      exit(1);
     }
-    SetFilePointer(File, Offset_Names, 0, FILE_BEGIN);
-    if (!ReadFile(File, EXPORT_NAME_TABLE.data(), (sizeof(DWORD) * EXPORT_DIRECTORY.NumberOfNames), 0, 0))
-    {
-        println("failed to read export name pointer table 0x{:0X}", GetLastError());
-        exit(1);
-    }
-    SetFilePointer(File, Offset_Ordinals, 0, FILE_BEGIN);
-    if (!ReadFile(File, EXPORT_ORDINAL_TABLE.data(), (sizeof(WORD) * EXPORT_DIRECTORY.NumberOfNames), 0, 0))
-    {
-        println("failed to read export ordinal table 0x{:0X}", GetLastError());
-        exit(1);
-    }
+  }, OptHeader);
 }
 
 auto GetExportOffsets() -> void
 {
-    int SECTION_INDEX = CalculateSectionLocation(OptionalHeader.DataDirectory[EXPORT].VirtualAddress);
-    Offset_Functions = EXPORT_DIRECTORY.AddressOfFunctions - SECTION_TABLE[SECTION_INDEX].VirtualAddress + SECTION_TABLE[SECTION_INDEX].PointerToRawData;
-    Offset_Ordinals = EXPORT_DIRECTORY.AddressOfNameOrdinals - SECTION_TABLE[SECTION_INDEX].VirtualAddress + SECTION_TABLE[SECTION_INDEX].PointerToRawData;
-    Offset_Names = EXPORT_DIRECTORY.AddressOfNames - SECTION_TABLE[SECTION_INDEX].VirtualAddress + SECTION_TABLE[SECTION_INDEX].PointerToRawData;
+  std::visit([](auto& header) {
+    int SECTION_INDEX = CalculateSectionLocation(
+        header.DataDirectory[EXPORT].VirtualAddress);
+
+    Offset_Functions = EXPORT_DIRECTORY.AddressOfFunctions -
+                       SECTION_TABLE[SECTION_INDEX].VirtualAddress +
+                       SECTION_TABLE[SECTION_INDEX].PointerToRawData;
+
+    Offset_Ordinals  = EXPORT_DIRECTORY.AddressOfNameOrdinals -
+                       SECTION_TABLE[SECTION_INDEX].VirtualAddress +
+                       SECTION_TABLE[SECTION_INDEX].PointerToRawData;
+
+    Offset_Names     = EXPORT_DIRECTORY.AddressOfNames -
+                       SECTION_TABLE[SECTION_INDEX].VirtualAddress +
+                       SECTION_TABLE[SECTION_INDEX].PointerToRawData;
+  }, OptHeader);
+}
+
+auto ReadExportTables() -> void
+{
+  EXPORT_TABLE.resize(EXPORT_DIRECTORY.NumberOfFunctions);
+  EXPORT_NAME_TABLE.resize(EXPORT_DIRECTORY.NumberOfNames);
+  EXPORT_ORDINAL_TABLE.resize(EXPORT_DIRECTORY.NumberOfNames);
+
+  File.seekg(Offset_Functions, std::ios_base::beg);
+  if (!File.read(reinterpret_cast<char*>(EXPORT_TABLE.data()),
+                 sizeof(DWORD) * EXPORT_DIRECTORY.NumberOfFunctions))
+  {
+    println("Failed to read function address table");
+    exit(1);
+  }
+
+  File.seekg(Offset_Names, std::ios_base::beg);
+  if (!File.read(reinterpret_cast<char*>(EXPORT_NAME_TABLE.data()),
+                 sizeof(DWORD) * EXPORT_DIRECTORY.NumberOfNames))
+  {
+    println("Failed to read export name pointer table");
+    exit(1);
+  }
+
+  File.seekg(Offset_Ordinals, std::ios_base::beg);
+  if (!File.read(reinterpret_cast<char*>(EXPORT_ORDINAL_TABLE.data()),
+                 sizeof(WORD) * EXPORT_DIRECTORY.NumberOfNames))
+  {
+    println("Failed to read export ordinal table");
+    exit(1);
+  }
 }
 
 auto ReadExportNames() -> void
 {
-    int SECTION_INDEX = CalculateSectionLocation(OptionalHeader.DataDirectory[EXPORT].VirtualAddress);
-    SetFilePointer(File, Offset_Names, 0, FILE_BEGIN);
+  std::visit([](auto& header) {
+    int SECTION_INDEX = CalculateSectionLocation(
+        header.DataDirectory[EXPORT].VirtualAddress);
+
+    File.seekg(Offset_Names, std::ios_base::beg);
     std::vector<DWORD> Pointers(EXPORT_DIRECTORY.NumberOfNames);
-    DWORD BytesRead = 0;
-    ReadFile(File, Pointers.data(), (sizeof(DWORD) * EXPORT_DIRECTORY.NumberOfNames), &BytesRead, 0);
-    for (int i = 0; i < EXPORT_DIRECTORY.NumberOfNames; i++)
+    File.read(reinterpret_cast<char*>(Pointers.data()),
+              sizeof(DWORD) * EXPORT_DIRECTORY.NumberOfNames);
+
+    for (int i = 0; i < (int)EXPORT_DIRECTORY.NumberOfNames; i++)
     {
-        DWORD nameOffset = Pointers[i] - SECTION_TABLE[SECTION_INDEX].VirtualAddress + SECTION_TABLE[SECTION_INDEX].PointerToRawData;
+      DWORD nameOffset = Pointers[i] -
+                         SECTION_TABLE[SECTION_INDEX].VirtualAddress +
+                         SECTION_TABLE[SECTION_INDEX].PointerToRawData;
 
-        SetFilePointer(File, nameOffset, 0, FILE_BEGIN);
+      File.seekg(nameOffset, std::ios_base::beg);
 
-        std::string name;
-        char c;
-        while (ReadFile(File, &c, sizeof(char), 0, 0) && c != '\0')
-            name += c;
-        names.push_back(name);
+      std::string name;
+      char c;
+      while (File.read(&c, sizeof(char)) && c != '\0')
+        name += c;
+      names.push_back(name);
     }
+  }, OptHeader);
 }
 
 auto ExportInfo(std::string_view Function) -> void
 {
-    int SECTION_INDEX = CalculateSectionLocation(OptionalHeader.DataDirectory[EXPORT].VirtualAddress);
-    auto i = std::ranges::find(names, Function);
-    int index = i - names.begin();
+  std::visit([&](auto& header) {
+    auto it = std::ranges::find(names, Function);
+    if (it == names.end())
+    {
+      println("Function '{}' not found in export table", Function);
+      return;
+    }
+
+    int index        = static_cast<int>(it - names.begin());
     auto OrdinalName = EXPORT_ORDINAL_TABLE[index];
-    println("FUNCTION: {}", names.at(index));
-    println("INDEX: {}", index);
+
+    println("FUNCTION: {}",   names.at(index));
+    println("INDEX: {}",      index);
     println("Name Ordinal: {}", OrdinalName);
-    println("Ordinal: {}", OrdinalName + EXPORT_DIRECTORY.Base);
-    println("RVA: {:0X}", EXPORT_TABLE[OrdinalName] - OptionalHeader.BaseOfCode);
+    println("Ordinal: {}",    OrdinalName + EXPORT_DIRECTORY.Base);
+    println("RVA: {:0X}",     EXPORT_TABLE[OrdinalName] - header.BaseOfCode);
+  }, OptHeader);
 }
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    if(argv[1] == nullptr)
-    {
-        print("Enter The .exe/dll name");
-        return 1;
-    }
-    LPSTR program = argv[1];
-    File = CreateFileA(program, GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-    if (File == INVALID_HANDLE_VALUE)
-    {
-        println("main failed to open {} 0x{:0X}", "", GetLastError());
-        return GetLastError();
-    }
+  File.open(argv[1], std::ios::binary);
+  if (!File.is_open())
+  {
+    println("Failed to open file");
+    return 1;
+  }
 
-    ReadDosHeader();
-    ReadFileHeader();
-    ReadOptionalHeader();
-    ReadSectionTable();
+  ReadNtHeaders();
+  ReadSectionTable();
+  ReadExportDirectory();
+  GetExportOffsets();
+  ReadExportNames();
+  ReadExportTables();
+  ExportInfo("NtAllocateVirtualMemory");
 
-    println("{}",(char*)SECTION_TABLE[0].Name);
-    //ReadExportDirectory(); 
-    //GetExportOffsets(); if it exists
-    //ReadExportNames();
-    //ReadExportTables();
-
-    CloseHandle(File);
-    return 0;
+  return 0;
 }
